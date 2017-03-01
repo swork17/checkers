4630c03cfc09f55dda9e7487423f0c72f06f70b1
Kevin Boucher <kevin.b1706@gmail.com>
2017-03-01 10:53:30 +0100

    Add message

--- src/com/checkers/main/Main.java
+++ src/com/checkers/main/Main.java
@@ -1,10 +1,10 @@
 package com.checkers.main;
 
 public class Main {
-
+	private static String TAG = "Cs : Main ";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Start Main");
 	}
 
 }
