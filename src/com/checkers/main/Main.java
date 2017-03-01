2297e874fd4b09bb4fa247af39988b5dca537bb4
Kevin Boucher <kevin.b1706@gmail.com>
2017-03-01 11:35:06 +0100

    Commentaire

--- src/com/checkers/main/Main.java
+++ src/com/checkers/main/Main.java
@@ -3,6 +3,10 @@ package com.checkers.main;
 public class Main {
 	private static String TAG = "Cs : Main ";
 
+	/**
+	 * Lancement de la procédure principale 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Start main");
 		
