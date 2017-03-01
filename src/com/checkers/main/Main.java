703f4adc9eedb0cd342e6fa7a4e728bd2c1e382d
Lanxss <fabien226@hotmail.fr>
2017-03-01 11:05:03 +0100

    Revert "Add message "
    This reverts commit 4630c03cfc09f55dda9e7487423f0c72f06f70b1.

--- src/com/checkers/main/Main.java
+++ src/com/checkers/main/Main.java
@@ -1,10 +1,10 @@
 package com.checkers.main;
 
 public class Main {
-	private static String TAG = "Cs : Main ";
-	
+
 	public static void main(String[] args) {
-		System.out.println("Start Main");
+		// TODO Auto-generated method stub
+
 	}
 
 }
